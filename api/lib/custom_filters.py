a59aa745b2f17be3238b4438f5c49047bb0cb5b7
Anthony Manning-Franklin <anthony@gigger.rocks>
2017-03-06 21:51:32 +0800

    Temporary Fix for parse error in reduce by distance

--- api/lib/custom_filters.py
+++ api/lib/custom_filters.py
@@ -1,5 +1,7 @@
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
+import traceback
+import sys
 
 M_IN_DEGREE = 111111
 
@@ -8,13 +10,17 @@ def reduce_by_distance(request, queryset):
     location = request.query_params.get('from_location', '').split(',')
     meters = request.query_params.get('max_distance_meters', '')
     degrees = meters_to_degrees(meters)
-    if len(location) * len(meters) > 0:
-        point = Point(float(location[0]), float(location[1]))
-        queryset = queryset.filter(
-            restaurant__location__dwithin=(
-                point, degrees
+    try:
+        if len(location) * len(meters) > 0:
+            point = Point(float(location[0]), float(location[1]))
+            queryset = queryset.filter(
+                restaurant__location__dwithin=(
+                    point, degrees
+                )
             )
-        )
+    except:
+        print("Could not reduce by distance")
+        print(sys.exc_info()[0])
     return queryset
 
 
