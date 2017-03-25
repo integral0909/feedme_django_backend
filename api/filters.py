082233224bb661d8e37ccbb76d3893e7c4499756
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-03-25 19:06:36 +0800

    Added fulfilment filtering server side.

--- api/filters.py
+++ api/filters.py
@@ -8,6 +8,10 @@ class Dish(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(name='price', lookup_expr='gte')
     max_price = django_filters.NumberFilter(name='price', lookup_expr='lte')
     keywords = django_filters.CharFilter(name='keywords__word')
+    has_delivery = django_filters.BooleanFilter(
+        name='restaurant__delivery_provider', lookup_expr='isnull', exclude=True)
+    has_booking = django_filters.BooleanFilter(
+        name='restaurant__quandoo_id', lookup_expr='isnull', exclude=True)
     order = django_filters.OrderingFilter(fields=(
         ('price', 'price'),
         ('distance', 'distance'),
