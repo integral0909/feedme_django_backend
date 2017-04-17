4c96d6565410f119213f82f2911d7623339ae7db
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-04-17 18:12:50 +0800

    Added management command to transfer all images from firebase to s3.

---
+++ main/management/commands/images_to_s3.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.conf import settings
+from main.models import *
+from main.management.commands.load_firebase_dump import run_chunked_iter
+import pyrebase
+import urllib
+import wget
+import boto3
+
+base = 'https://firebasestorage.googleapis.com/v0/b/feedmee-appsppl-dev.appspot.com/o/'
+s3_client = boto3.client('s3')
+config = {
+  "apiKey": "AIzaSyDJdcBZSwLlpWENN5oWZYQVZL0u7ZPSzhc",
+  "authDomain": "feedmee-appsppl-dev.firebaseapp.com",
+  "databaseURL": "https://feedmee-appsppl-dev.firebaseio.com",
+  "storageBucket": "feedmee-appsppl-dev.appspot.com",
+}
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+
+def _download_imgs(rests):
+    for rest in rests:
+        dl_url = urllib.parse.unquote(rest.image_url).replace(base, '').split('?')[0]
+        filename = dl_url.split('/')[-1]
+        try:
+            if dl_url[:6] == 'images':
+                storage.child(dl_url).download(settings.TMP_PATH + filename)
+            else:
+                    wget.download(dl_url, settings.TMP_PATH + filename)
+        except ValueError:
+            print('URL invalid', dl_url)
+        except:
+            print('Unknown error')
+        else:
+            s3_client.upload_file(settings.TMP_PATH + filename, 'fdme-raw-img',
+                                  filename)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        rests = Restaurant.objects.all()
+        dishes = Dish.objects.all()
+        run_chunked_iter(rests, _download_imgs)
+        run_chunked_iter(dishes, _download_imgs)
