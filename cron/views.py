7f5e49522154a6b3dd30bb69e70b1fc63cc70cfb
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-05-08 16:35:54 +0800

    Added worker env check to cron views to prevent DDoS attack vector.

--- cron/views.py
+++ cron/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.core.management import call_command
 from django.views.decorators.csrf import csrf_exempt
@@ -5,13 +6,23 @@ from django.http import HttpResponse, HttpResponseServerError, HttpResponseBadRe
 from main.models import Dish
 
 
+def worker(func):
+    def wrapper(request):
+        if os.environ.get('ROLE', '') == 'WORKER':
+            return func(request)
+        return HttpResponseBadRequest('BAD REQUEST')
+    return wrapper
+
+
 @csrf_exempt
+@worker
 def backup_db(request):
     call_command('dbbackup')
     return HttpResponse('OK')
 
 
 @csrf_exempt
+@worker
 def randomise_dishes(request):
     for dish in Dish.objects.all():
         dish.randomise()
