9a99383ac145cbc0e5ff11a5a855109112ca799f
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-06-22 11:20:16 +0800

    Add ingredients to search terms.

--- api/views.py
+++ api/views.py
@@ -241,7 +241,8 @@ class SearchTermList(APIView):
         res = {
             'keywords': [kwd.word for kwd in models.Keyword.objects.all()],
             'highlights': [hlt.name for hlt in models.Highlight.objects.all()],
-            'tags': [tag.name for tag in models.Tag.objects.all()]
+            'tags': [tag.name for tag in models.Tag.objects.all()],
+            'ingredients': [i.name for i in models.Ingredient.objects.all()]
         }
         count = 0
         for key, val in res.items():
