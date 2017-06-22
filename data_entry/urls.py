e6053d19efffbf822be8997f5bed6a581f1aa1aa
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-06-22 22:01:25 +0800

    Refactoring and new features for data entry app. Removed restaurant only urls and view functions. Removed breadcrumbs. Improved styles.

--- data_entry/urls.py
+++ data_entry/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^restaurants/add/', views.add_restaurant),
-    url(r'^restaurants/(?P<rest_id>[0-9]+)/change/(?P<tab>[\w]+)/', views.change_restaurant),
-    url(r'^restaurants/(?P<rest_id>[0-9]+)/change/', views.change_restaurant),
+    url(r'^(?P<item_type>[\w]+)/(?P<item_id>[0-9]+)/change/(?P<tab>[\w]+)/', views.change_item),
     url(r'^(?P<item_type>[\w]+)/(?P<item_id>[0-9]+)/change/', views.change_item),
     url(r'^(?P<item_type>[\w]+)/add/', views.change_item),
-    url(r'^(?P<list_type>[\w]+)/', views.list_items),
+    url(r'^(?P<item_type>[\w]+)/', views.list_items),
 ]
--- data_entry/views.py
+++ data_entry/views.py
@@ -11,71 +11,16 @@ from common.utils import merge_dicts
 
 
 @staff_member_required
-def change_restaurant(request, rest_id=None, tab=False):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = RestaurantForm(request.POST)
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/data-entry/restaurants/%s/change/' % rest.id)
-    # if a GET (or any other method) we'll create a blank form
-    rest = get_object_or_404(Restaurant, pk=rest_id)
-    context = {
-        'form': RestaurantForm(instance=rest), 'tab': tab, 'action': 'Change',
-        'blogs': Blog.objects.filter(restaurant=rest), 'logs': rest.get_logs(),
-        'dishes': Dish.objects.filter(restaurant=rest),
-        'blogform': BlogForm(initial={'restaurant_id': rest.id}),
-        'dishform': DishForm(initial={'restaurant': rest}),
-        'breadcrumbs': [
-            {'url': '/data-entry/restaurants/', 'name': 'Restaurants'},
-            {'url': '/data-entry/restaurants/%s/change/' % rest.id, 'name': rest.name}
-        ]
-    }
-    return render(request, 'de_restaurant.html', context)
-
-
-@staff_member_required
-def add_restaurant(request):
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = RestaurantForm(request.POST)
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/data-entry/restaurants/%s/change/' % rest.id)
-    context = {
-        'form': RestaurantForm(), 'logs': [], 'tab': False, 'action': 'Add',
-        'breadcrumbs': [
-            {'url': '/data-entry/restaurants/', 'name': 'Restaurants'},
-            {'url': '/data-entry/restaurants/add/', 'name': 'Add Restaurant'}
-        ]
-    }
-    return render(request, 'de_restaurant.html', context)
-
-
-@staff_member_required
-def list_items(request, list_type):
-    class_name, ObjClass, FormClass = _get_class_objects(list_type)
+def list_items(request, item_type):
+    class_name, ObjClass, FormClass = _get_class_objects(item_type)
     paginator = Paginator(ObjClass.objects.all(), 100)
     items = _paginate(paginator, request.GET.get('page'))
-    crumbs = [
-        {'url': '/data-entry/%s/' % list_type, 'name': list_type.title()}
-    ]
-    return render(request, 'de_%s_list.html' % list_type, {list_type: items,
-                                                           'breadcrumbs': crumbs})
+    return render(request, 'de_%s_list.html' % item_type, {item_type: items})
 
 
 @staff_member_required
-def change_item(request, item_type, item_id=None):
+def change_item(request, item_type, item_id=None, tab=False):
     class_name, ObjClass, FormClass = _get_class_objects(item_type)
-    crumbs = [
-        {'url': '/data-entry/%s/' % item_type, 'name': item_type.title()},
-    ]
-    context = {}
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = FormClass(request.POST)
@@ -88,20 +33,12 @@ def change_item(request, item_type, item_id=None):
     # if a GET (or any other method) we'll create a blank form
     if item_id:
         obj = get_object_or_404(ObjClass, pk=item_id)
-        context = {'action': 'Change', 'logs': obj.get_logs(),
-                   'form': FormClass(instance=obj)}
-        context = merge_dicts(context, _extra_processing(item_type, obj))
-        crumbs.append({
-            'url': '/data-entry/%s/%s/change/' % (item_type, obj.id), 'name': str(obj)
-        })
+        context = merge_dicts({'action': 'Change', 'form': FormClass(instance=obj),
+                               'logs': obj.get_logs()}, _extra_processing(item_type, obj))
     else:
-        context = merge_dicts(context, _extra_processing(item_type))
-        crumbs.append({
-            'url': '/data-entry/%s/add/' % item_type,
-            'name': 'Add %s' % item_type.title()
-        })
+        context = _extra_processing(item_type)
 
-    defaults = {'form': FormClass(), 'breadcrumbs': crumbs, 'action': 'Add', 'logs': []}
+    defaults = {'form': FormClass(), 'action': 'Add', 'logs': [], 'tab': tab}
     context = merge_dicts(defaults, context)
     return render(request, 'de_%s.html' % class_name.lower(), context)
 
@@ -114,6 +51,13 @@ def _extra_processing(item_type, obj=None):
                                              exclude=('recipe', ))
         ctx['formset'] = RecipeFormSet(
             queryset=RecipeIngredient.objects.filter(recipe=obj))
+    if item_type == 'restaurants' and obj:
+        ctx = {
+            'blogs': Blog.objects.filter(restaurant=obj),
+            'dishes': Dish.objects.filter(restaurant=obj),
+            'blogform': BlogForm(initial={'restaurant_id': obj.id}),
+            'dishform': DishForm(initial={'restaurant': obj}),
+        }
     return ctx
 
 
@@ -134,9 +78,3 @@ def _paginate(paginator, page):
         return paginator.page(1)
     except EmptyPage:
         return paginator.page(paginator.num_pages)
-
-
-"""
->>> from myapp.models import Author
->>> AuthorFormSet = modelformset_factory(Author, fields=('name', 'title'))
-formset = AuthorFormSet(queryset=Author.objects.filter(name__startswith='O'))"""
