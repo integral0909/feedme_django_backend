20bf41f175d3c51e48b0ce0ad19517e043c8fe08
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-07-21 10:41:08 +0800

    Refactor and simplify JobMessageView class.

--- django_sqs_jobs/views.py
+++ django_sqs_jobs/views.py
@@ -3,31 +3,47 @@ from django_sqs_jobs.jobs import Job
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import HttpResponse, HttpResponseBadRequest
-
+import json
 # Need to define a single view that will receive messages.
 # It's up to consumer to route requests
 # Find consumer's jobs via
 # for job in Job.__subclasses__():
 
+class JobMessageException(Exception):
+    pass
+
+
+class InvalidContentType(JobMessageException):
+    pass
+
+
+class EmptyRequestBody(JobMessageException):
+    pass
+
+
+class PostBodyParseError(JobMessageException):
+    pass
+
 
 @method_decorator(csrf_exempt, name='dispatch')
-class SQSMessageView(View):
+class JobMessageView(View):
     allowed_methods = ['post', 'options']
 
     def post(self, request):
-        response = self.validate_post_body(request)
-        if isinstance(response, HttpResponse):  # This pattern sucks. Use exceptions instead.
-            return response
+        try:
+            self.validate_post_body(request)
+            self.data = self.process_post_body(request)
+        except JobMessageException as e:
+            return HttpResponseBadRequest(str(e))
+        else:
+            return self.execute_job(data)
 
-        data = self.process_post_body(request)
-        if isinstance(data, HttpResponse):
-            return data
-
-        return self.execute_job(data)
-
-    def execute_job(self, data):
-        job = [j for j in Job.__subclasses__() if j.__class__.__name__ == data['JOB']][0]
-        job(*data['ARGS'], **data['KWARGS'])
+    def execute_job(self):
+        """Execute the Job specified in the request"""
+        job = [j for j in Job.__subclasses__()
+               if j.__class__.__name__ == self.data['JOB']][0]()
+        job(*self.data['ARGS'], **self.data['KWARGS'])
+        return HttpResponse('OK')
 
 
     def validate_post_body(self, request, content_type='application/json'):
@@ -35,9 +51,9 @@ class SQSMessageView(View):
         Verifies that the request has a valid body and content type, default JSON.
         """
         if len(request.body) == 0:
-            return HttpResponseBadRequest('Empty request body')
+            raise InvalidContentType('Empty request body')
         if content_type not in request.content_type:
-            return HttpResponseBadRequest('Invalid Content-Type: %s only' % content_type)
+            raise EmptyRequestBody('Invalid Content-Type: %s only' % content_type)
         return True
 
     def process_post_body(self, request):
@@ -50,4 +66,4 @@ class SQSMessageView(View):
             return json.loads(request.body)
         except Exception as e:
             json_data = {'error': '{0}'.format(str(e))}
-            return HttpResponseBadRequest(json.dumps(json_data), content_type="application/json")
+            raise PostBodyParseError(json.dumps(json_data))
