b481a63c4ff7e9cec1255da9c342d27c86132687
Anthony Manning-Franklin <anthony@feedmeeapp.com>
2017-08-18 16:58:19 +0800

    Privacy policy url redirect.

--- webapp/flatpages/urls.py
+++ webapp/flatpages/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^about/$', RedirectView.as_view(url='/impact/', permanent=True)),
     url(r'^terms\.html$', RedirectView.as_view(url='/terms/', permanent=True)),
     url(r'^privacy\.html$', RedirectView.as_view(url='/privacy/', permanent=True)),
+    url(r'^privacy-policy/$', RedirectView.as_view(url='/privacy/', permanent=True)),
     url(r'^donation\.html$', RedirectView.as_view(url='/donation/', permanent=True)),
     url(r'^events\.html$', RedirectView.as_view(url='/events/', permanent=True)),
     url(r'^team\.html$', RedirectView.as_view(url='/team/', permanent=True)),
